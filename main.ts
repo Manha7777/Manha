67b286060b8a6b9853b52272c37eedb936779ca7
Manha7777 <134036955+Manha7777@users.noreply.github.com>
2023-05-19 09:23:20 +0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,66 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    AESTROID = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . 7 . . 7 . . . . . . . . 
+        . . . . 7 . . 7 . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . 7 . . . . . . 
+        . . 7 7 . . . . . 7 . . . . . . 
+        . . . 7 . . . . 7 7 . . . . . . 
+        . . . 7 7 7 7 7 7 . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, mySprite, 50, 50)
+})
+let AESTROID: Sprite = null
+let mySprite: Sprite = null
+effects.starField.startScreenEffect()
+mySprite = sprites.create(img`
+    . . . . f f f f f . . . . . . . 
+    . . . f e e e e e f . . . . . . 
+    . . f d d d d e e e f . . . . . 
+    . c d f d d f d e e f f . . . . 
+    . c d f d d f d e e d d f . . . 
+    c d e e d d d d e e b d c . . . 
+    c d d d d c d d e e b d c . . . 
+    c c c c c d d e e e f c . . . . 
+    . f d d d d e e e f f . . . . . 
+    . . f e e e f f e e e f . . . . 
+    . . f f f f f e e e e e f . f f 
+    . . f d b f e e f f e e f . e f 
+    . f f d d f e f f e e e f . e f 
+    . f f f f f f e b b f e f f e f 
+    . f d d f e e e d d b e f f f f 
+    . . f f f f f f f f f f f f f . 
+    `, SpriteKind.Player)
+mySprite.setPosition(81, 111)
+controller.moveSprite(mySprite, 100, 0)
+mySprite.setStayInScreen(true)
+game.onUpdateInterval(500, function () {
+    AESTROID = sprites.createProjectileFromSide(img`
+        . . . . . . . . . c c 8 . . . . 
+        . . . . . . 8 c c c f 8 c c . . 
+        . . . c c 8 8 f c a f f f c c . 
+        . . c c c f f f c a a f f c c c 
+        8 c c c f f f f c c a a c 8 c c 
+        c c c b f f f 8 a c c a a a c c 
+        c a a b b 8 a b c c c c c c c c 
+        a f c a a b b a c c c c c f f c 
+        a 8 f c a a c c a c a c f f f c 
+        c a 8 a a c c c c a a f f f 8 a 
+        . a c a a c f f a a b 8 f f c a 
+        . . c c b a f f f a b b c c 6 c 
+        . . . c b b a f f 6 6 a b 6 c . 
+        . . . c c b b b 6 6 a c c c c . 
+        . . . . c c a b b c c c . . . . 
+        . . . . . c c c c c c . . . . . 
+        `, 0, 50)
+    AESTROID.x = randint(0, scene.screenWidth())
+    AESTROID.setKind(SpriteKind.Enemy)
+})
